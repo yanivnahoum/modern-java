179d87feaa318b9b5696ec159addfc1aa67f5c49
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2023-03-15 13:51:46 +0200

    Polished MethodReference

--- modern-java-examples/src/main/java/com/att/tlv/training/java/lambdas/MethodReference.java
+++ modern-java-examples/src/main/java/com/att/tlv/training/java/lambdas/MethodReference.java
@@ -7,81 +7,77 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class MethodReference {
-    
-    public void foo() {
+
+    public void instanceOrStatic() {
         ExecutorService executor = Executors.newSingleThreadExecutor();
-        
+
         // Simple lambda calling an existing method with the correct signature (void run())
         executor.execute(() -> doSomething());
-        
+
         // 1. Reference to an instance method of a specific object
         executor.execute(this::doSomething);
-        
-        // 2. Reference to a static method
+
+        // 2. Reference to a static method of a specific type
         executor.execute(MethodReference::doSomethingElse);
-        
+
         // cleanup
         shutdown(executor);
     }
-    
+
     private void doSomething() {
         System.out.println(Thread.currentThread().getName() + ": doSomething");
     }
-    
+
     private static void doSomethingElse() {
         System.out.println(Thread.currentThread().getName() + ": doSomethingElse");
-    }    
-    
-    public void moo() {
+    }
+
+    public void constructor() {
         var names = List.of("a", "b", "c");
-        
+
         // Calling the constructor with a lambda
         names.forEach(s -> new Printer(s));
-        
+
         // 3. Reference to a constructor
         names.forEach(Printer::new);
     }
-    
-    
-    public void bar() {
-        List<String> names = new ArrayList<>();
-        names.add("carl");
-        names.add("");
-        names.add("alice");
-        names.add("");
-        names.add("Bob");
-        names.add("");
-        
+
+
+    public void arbitrary() {
+        var names = new ArrayList<>(
+                List.of("Carl", "", "Alice", "", "Bob", "")
+        );
+
 //        names.removeIf(s -> s.isEmpty());
-        
-        // 4. Reference to an instance method of a arbitrary object supplied later
+
+        // 4. Reference to an instance method of an arbitrary object of a particular type
         names.removeIf(String::isEmpty);
-        
+
         System.out.println(names);
     }
-    
+
     public void shutdown(ExecutorService executor) {
         try {
+            executor.shutdown();
             executor.awaitTermination(1, TimeUnit.SECONDS);
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        executor.shutdown();
     }
-    
+
     static class Printer {
-        
+
         public Printer(String text) {
             System.out.println(text);
         }
+
         public Printer(String text, String text2) {
             System.out.println(text + text2);
         }
     }
-    
+
     public static void main(String[] args) {
         MethodReference methodReference = new MethodReference();
-        methodReference.bar();
+        methodReference.instanceOrStatic();
     }
 }
--- modern-java-exercises/src/main/java/com/att/tlv/training/java/exercises/lambdas/MethodReferences.java
+++ modern-java-exercises/src/main/java/com/att/tlv/training/java/exercises/lambdas/MethodReferences.java
@@ -1,9 +1,10 @@
 package com.att.tlv.training.java.exercises.lambdas;
 
-import com.att.tlv.training.java.exercises.Exercises;
 import com.att.tlv.training.java.exercises.data.Person;
 
 import java.util.function.Predicate;
+
+import static com.att.tlv.training.java.exercises.Exercises.replaceThisWithSolution;
 
 public class MethodReferences {
 
@@ -14,7 +15,7 @@ public class MethodReferences {
      */    
     public static Predicate<String> buildIsEmpty() {
         // TODO return a method reference here!
-        return Exercises.replaceThisWithSolution();
+        return replaceThisWithSolution();
     }
     
     /**
@@ -23,7 +24,7 @@ public class MethodReferences {
      */    
     public static Predicate<Person> buildIsAMinor() {
         // TODO Add a private method isAMinor() and return a reference to it.
-        return Exercises.replaceThisWithSolution();
+        return replaceThisWithSolution();
     }
 }
   
