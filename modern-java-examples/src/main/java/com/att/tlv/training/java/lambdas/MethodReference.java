a02f801b631e9673819253d3a095c7c6efc125b8
Yaniv Nahoum <yaniv.nahoum@gmail.com>
2024-05-20 08:59:15 +0300

    Used ExecutorService as autoCloseable

--- modern-java-examples/src/main/java/com/att/tlv/training/java/lambdas/MethodReference.java
+++ modern-java-examples/src/main/java/com/att/tlv/training/java/lambdas/MethodReference.java
@@ -9,19 +9,17 @@ import java.util.concurrent.TimeUnit;
 public class MethodReference {
 
     public void instanceOrStatic() {
-        ExecutorService executor = Executors.newSingleThreadExecutor();
+        try(ExecutorService executor = Executors.newSingleThreadExecutor()) {
 
-        // Simple lambda calling an existing method with the correct signature (void run())
-        executor.execute(() -> doSomething());
+            // Simple lambda calling an existing method with the correct signature (void run())
+            executor.execute(() -> doSomething());
 
-        // 1. Reference to an instance method of a specific object
-        executor.execute(this::doSomething);
+            // 1. Reference to an instance method of a specific object
+            executor.execute(this::doSomething);
 
-        // 2. Reference to a static method of a specific type
-        executor.execute(MethodReference::doSomethingElse);
-
-        // cleanup
-        shutdown(executor);
+            // 2. Reference to a static method of a specific type
+            executor.execute(MethodReference::doSomethingElse);
+        }
     }
 
     private void doSomething() {
@@ -54,15 +52,6 @@ public class MethodReference {
         names.removeIf(String::isEmpty);
 
         System.out.println(names);
-    }
-
-    public void shutdown(ExecutorService executor) {
-        try {
-            executor.shutdown();
-            executor.awaitTermination(1, TimeUnit.SECONDS);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
 
     static class Printer {
